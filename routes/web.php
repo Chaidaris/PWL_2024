52fd23e16023221363cdf2ed680bac6c78aea702
Chaidaris <2131710102@student.polinema.ac.id>
2024-02-22 15:01:53 +0700

    update jobsheet2 controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,25 +19,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/index', [PageController::class,'index']);
 
-Route::get('/hello', function () {
-    return 'Hello World';
-    });
+Route::get('/hello', [WelcomeController::class,'hello']);
 
 Route::get('/world', function () {
     return 'World';
     });
 
-Route::get('/welcome', function () {
-    return "Selamat Datang";
-    });
-
-Route::get('/about', function () {
-    return "NIM : 21417102" . "<br>" . "Nama : Achmad Chaidar Ismail";
-    });
+Route :: get('/about', [PageController::class,'about']);
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya '.$name;
@@ -41,11 +37,22 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
     });
 
-Route::get('/articles/{article}', function ($article) {
-    return 'Halaman artikel dengan ID '.$article;
-    });
+Route::get('/articles/{id}', [PageController::class,'articles']);
         
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
     });
 
+Route::get('/home', [HomeController::class,'index']);
+
+Route::get('/about', [AboutController::class,'about']);
+
+Route::get('/articles/{id}', [ArticlesController::class,'articles']);
+
+Route::resource('photos', PhotoController::class);
+// Route::resource('photos', PhotoController::class)->only([
+//     'index', 'show'
+// ]);
+// Route::resource('photos', PhotoController::class)->except([
+//     'create', 'store', 'update', 'destroy'
+// ]);
