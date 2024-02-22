fc4ed752536867f1b2e17db7f3c60aac4fd196bb
Chaidaris <2131710102@student.polinema.ac.id>
2024-02-22 14:34:23 +0700

    update jobsheet2 routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+    });
+
+Route::get('/world', function () {
+    return 'World';
+    });
+
+Route::get('/welcome', function () {
+    return "Selamat Datang";
+    });
+
+Route::get('/about', function () {
+    return "NIM : 21417102" . "<br>" . "Nama : Achmad Chaidar Ismail";
+    });
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+        
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+Route::get('/articles/{article}', function ($article) {
+    return 'Halaman artikel dengan ID '.$article;
+    });
+        
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+
